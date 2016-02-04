4af1c198c4246854e797c46d44e6353ca86eb185
Pieter De Praetere <pieter.de.praetere@helptux.be>
2016-02-04 11:31:45 +0100

    Model for User Reports

--- scoremodel/models/public.py
+++ scoremodel/models/public.py
@@ -1,20 +1,30 @@
 from scoremodel import db
+from scoremodel.models.general import Question, Answer, Report
+from scoremodel.models.user import User
+
+
+class UserReport(db.Model):
+    __tablename__ = 'UserReport'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    report_date = db.Column(db.Date)
+    report_id = db.Column(db.Integer, db.ForeignKey(Report.id))
+    questions = db.relationship('QuestionAnswer', backref='report', lazy='dynamic')
 
 
 class QuestionAnswer(db.Model):
-    __tablename__ = 'question_answers'
+    __tablename__ = 'QuestionAnswer'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    question_id = db.Column(db.Integer, db.ForeignKey(Question.id))
+    answer_id = db.Column(db.Integer, db.ForeignKey(Answer.id))
+    user_report_id = db.Column(db.Integer, db.ForeignKey(UserReport.id))
     # question_id
     # user_id
     # answer_id
     # user_report_id
 
 
-class UserReport(db.Model):
-    __tablename__ = 'user_reports'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     # user_id
     # report_name
     # report_id
